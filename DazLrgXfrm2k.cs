8ee7fed043297dc5d37ad18644dffdfa9272d912
SirDiazo <diazo@hotmail.com>
2021-10-08 21:53:25 -0600

    Initial commit

---
+++ DazLrgXfrm2k.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KMod;
+using HarmonyLib;
+using UnityEngine;
+
+
+namespace DazLrgXfrm2k
+{
+    public class DazPatches : UserMod2 //mod loading class
+    {
+        [HarmonyPatch(typeof(PowerTransformerConfig), "CreateBuildingDef")]
+        public static class DazPwrXfrmLrg2kPatch
+        {
+            internal static void Postfix(ref BuildingDef __result)
+            {
+                Debug.Log("Daz xfomre 2k");
+                __result.GeneratorWattageRating = 2000f;
+                __result.GeneratorBaseCapacity = 2000f;
+            }
+        }
+    }
+}
